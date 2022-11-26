408e8f3393af50130c482339188c55e496153478
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-26 13:42:55 -0300

    yes

--- Training.py
+++ Training.py
@@ -4,5 +4,5 @@ months = ["january","february","march","april","may","june"]
 
 values = ["10000","20000","40000","80000","160000","320000"]
 
-matplotlib.pyplot.show()
 matplotlib.pyplot.plot(months, values)
+matplotlib.pyplot.show()
