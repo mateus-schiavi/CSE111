6dba47e142ff5cbd76009f9e0e9f91fce48a9455
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-26 13:41:01 -0300

    ty

---
+++ Training.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot
+
+months = ["january","february","march","april","may","june"]
+
+values = ["10000","20000","40000","80000","160000","320000"]
+
+matplotlib.pyplot.plot(months, values)
