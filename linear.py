2421d9469105ca14edd2e05934facc95c858b541
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 19:51:59 -0300

    yes

--- linear.py
+++ linear.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 
 def test_number():
-    data = np.arange(4) + 1j*np.arange(-2, 2) 
+    data = np.arange() + 1j*np.arange(2, 10) 
     x = data.real 
     y = data.imag 
     plt.plot(x, y, '-.r*') 
