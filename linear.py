a4a7d1aaa846ea509b32aa4f452a7e0d9b042aeb
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 20:09:38 -0300

    y

--- linear.py
+++ linear.py
@@ -6,13 +6,12 @@ def test_number():
     data = np.arange(8) + 1j*np.arange(2, 10) 
     x = data.real 
     y = data.imag
-    
-def test_plot():
-    x = data.real 
-    y = data.imag
     plt.plot(x, y, '-.r*') 
     plt.ylabel('Imaginary') 
     plt.xlabel('Real') 
     plt.show()
 
+def test_plot():
+    plt.show()
+    print("")
 pytest.main(["-v", "--tb=line", "-rN", __file__])
