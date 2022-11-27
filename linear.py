8c89d8eb76fd002d55baf7241a52b766cb695125
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 19:34:58 -0300

    yes

--- linear.py
+++ linear.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import pytest
-data = np.arange(8) + 1j*np.arange(-4, 4) 
-x = data.real 
-y = data.imag 
-plt.plot(x, y, '-.r*') 
-plt.ylabel('Imaginary') 
-plt.xlabel('Real') 
-plt.show()
+
+def test_number():
+    data = np.arange(8) + 1j*np.arange(-4, 4) 
+    x = data.real 
+    y = data.imag 
+    plt.plot(x, y, '-.r*') 
+    plt.ylabel('Imaginary') 
+    plt.xlabel('Real') 
+    plt.show()
 
 pytest.main(["-v", "--tb=line", "-rN", __file__])
