edec209f487ab57ff252dd44b17b53eee3cf4f74
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 22:50:06 -0300

    yes

--- linear.py
+++ linear.py
@@ -4,17 +4,12 @@ import pytest
 
 def test_number():
     data = np.arange(8) + 1j*np.arange(2, 10) 
+
+def x_y():
     x = data.real 
     y = data.imag
     plt.plot(x, y, '-.r*') 
     plt.ylabel('Imaginary') 
     plt.xlabel('Real') 
-    plt.show()
-    
-def test_print():
-    if plt.show():
-        print("Successful")
-    else:
-        print("Error")
-        
+    plt.show()        
 pytest.main(["-v", "--tb=line", "-rN", __file__])
