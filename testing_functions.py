be58ef06111812fdf01663839106310fb4f6a999
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 22:58:18 -0300

    yes

---
+++ testing_functions.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+import pytest
+
+data = np.arange() + 1j*np.arange()
+x = data.real 
+y = data.imag
+plt.plot(x, y, '-.r*') 
+plt.ylabel('Imaginary') 
+plt.xlabel('Real') 
+plt.show()  
+pytest.main(["-v", "--tb=line", "-rN", __file__])
