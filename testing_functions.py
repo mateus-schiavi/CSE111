6aaef8a86afb181e2c4b262f0b91a18474fe82e3
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 23:04:50 -0300

    yes

--- testing_functions.py
+++ testing_functions.py
@@ -2,13 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
-def test_range():
-    data = np.arange(0) + 1j*np.arange(0,0)
+def figure_1():
+    data = np.arange(2) + 1j*np.arange(1,3)
     x = data.real 
     y = data.imag
     plt.plot(x, y, '-.r*') 
     plt.ylabel('Imaginary') 
     plt.xlabel('Real') 
     plt.show()
-      
+
+def figure_2():
+    data = np.arange(5) + 1j*np.arange(2,7)
+    x = data.real 
+    y = data.imag
+    plt.plot(x, y, '-.r*') 
+    plt.ylabel('Imaginary') 
+    plt.xlabel('Real') 
+    plt.show()
+
+def figure_3():
+    data = np.arange(8) + 1j*np.arange(3,11)
+    x = data.real 
+    y = data.imag
+    plt.plot(x, y, '-.r*') 
+    plt.ylabel('Imaginary') 
+    plt.xlabel('Real') 
+    plt.show()
+
 pytest.main(["-v", "--tb=line", "-rN", __file__])
