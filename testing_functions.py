79798f40a8d04f943978cb1435c543360062b8e1
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 23:12:46 -0300

    yes

--- testing_functions.py
+++ testing_functions.py
@@ -9,5 +9,20 @@ def test_number():
     plt.plot(x, y, '-.r*') 
     plt.ylabel('Imaginary') 
     plt.xlabel('Real') 
-    plt.show()  
+    plt.show()
+    
+def second_frame():
+    # make data
+    x = np.linspace(0, 10, 100)
+    y = 4 + 2 * np.sin(2 * x)
+
+    # plot
+    fig, ax = plt.subplots()
+
+    ax.plot(x, y, linewidth=2.0)
+
+    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+        ylim=(0, 8), yticks=np.arange(1, 8))
+
+    plt.show()
 pytest.main(["-v", "--tb=line", "-rN", __file__])
