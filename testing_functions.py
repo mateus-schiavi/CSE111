17d830f1ffb2952c9b53291c8f6fa36148c57515
Mateus Schiavi <mtsschiavi@gmail.com>
2022-11-27 23:06:16 -0300

    yes

--- testing_functions.py
+++ testing_functions.py
@@ -11,22 +11,4 @@ def figure_1():
     plt.xlabel('Real') 
     plt.show()
 
-def figure_2():
-    data = np.arange(5) + 1j*np.arange(2,7)
-    x = data.real 
-    y = data.imag
-    plt.plot(x, y, '-.r*') 
-    plt.ylabel('Imaginary') 
-    plt.xlabel('Real') 
-    plt.show()
-
-def figure_3():
-    data = np.arange(8) + 1j*np.arange(3,11)
-    x = data.real 
-    y = data.imag
-    plt.plot(x, y, '-.r*') 
-    plt.ylabel('Imaginary') 
-    plt.xlabel('Real') 
-    plt.show()
-
 pytest.main(["-v", "--tb=line", "-rN", __file__])
