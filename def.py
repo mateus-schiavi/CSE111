53b50a406d7fc09c8d396710ed84595ea16a2449
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 19:49:46 -0300

    yes

--- def.py
+++ def.py
@@ -7,3 +7,9 @@ a = int(input("Please enter the real number: "))
 b = int(input("Please enter the imaginary number: "))
 print(complex(a + 1j*b))
 
+data = np.arange(2) + 1j*np.arange(3,5)
+x = data.real 
+y = data.imag
+plt.plot(x, y, '-.r*') 
+plt.ylabel('Imaginary') 
+plt.xlabel('Real') 
