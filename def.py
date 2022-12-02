dfc34012c2efa7852db8c18fe19e37425beb9801
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 19:40:53 -0300

    yes

--- def.py
+++ def.py
@@ -3,7 +3,9 @@ import numpy as np
 import cmath
 import pytest
 
-a = int(input("Please enter the real number: "))
-b = int(input("Please enter the imaginary number: "))
-print(complex(a + 1j*b))
-    
+X = int(input("Please enter the real number: "))
+y = int(input("Please enter the imaginary number: "))
+print(complex(x + 1j*y))
+x = data.real
+y = data.imag
+plt.plot(x, y, '-.r*') 
