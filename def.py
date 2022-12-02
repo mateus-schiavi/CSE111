f8acbe9cf44b7ab81d8df7a69669254a5f66bd2b
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 18:55:08 -0300

    yes

--- def.py
+++ def.py
@@ -3,9 +3,7 @@ import numpy as np
 import cmath
 import pytest
 
-def get_number():
-    a = int(input("Please enter the real number: "))
-    b = int(input("Please enter the imaginary number: "))
-    c = complex(a + b)
+a = int(input("Please enter the real number: "))
+b = int(input("Please enter the imaginary number: "))
+c = complex(a + b)
     
-pytest.main(["-v", "--tb=line", "-rN", __file__])
