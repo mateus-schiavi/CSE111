e8eba8bc2afabbca07c8b1d9ca515fa985950973
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 21:33:51 -0300

    yes

--- def.py
+++ def.py
@@ -7,9 +7,13 @@ x= int(input("Please enter the real number of the equation: "))
 y = int(input("Please enter the imaginary number of the equation: "))
 print(complex(x + 1j*y))
 
+x1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) 
+y1 = x + 1j*y
 
 plt.plot(x,y, '-.r*')
 plt.xlabel('Real')
 plt.ylabel('Imaginary') 
 plt.show()
 plt.figure()
+plt.plot(x1, y1, '-.') 
+plt.show() 
