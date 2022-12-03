667a1a257857321a281bf8edf29f059e72d1d723
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-02 21:06:28 -0300

    yes

--- def.py
+++ def.py
@@ -7,5 +7,5 @@ x= int(input("Please enter the real number of the equation: "))
 y = int(input("Please enter the imaginary number of the equation: "))
 print(complex(x + 1j*y))
 
-plt.plot(complex)
+plt.plot(x + 1j*y)
 plt.show()
