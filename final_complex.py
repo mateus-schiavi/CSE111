491802f2dd5153f45d9a540524a8e3aec646154f
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-03 14:19:33 -0300

    yes

--- final_complex.py
+++ final_complex.py
@@ -3,11 +3,37 @@ import numpy as np
 import pytest
 import cmath
 
-x = int(input("Enter the real number of the equation: "))
-y = int(input("Enter the real number of the equation: "))
-print(complex(x + 1j*y))
+running = True;
 
-plt.plot(x,y,'k--')
-plt.xlabel('Real')
-plt.ylabel('Imaginary')
-plt.show()
+print("WELCOME TO COMPLEX NUMBERS CALCULATOR")
+print("|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|")
+
+def main():
+    x = int(input("Enter the real part of the equation: "))
+    y = int(input("Enter the imaginary part of the equation: "))
+    
+    z = complex(x+1j*y)
+    print(z)
+    
+    repeat()
+    
+def repeat():
+    answer = input("Would you like to compute again? ")
+    if answer.lower() == "y":
+        main()
+    if answe.lower() == "n":
+        print("The program will now end!")
+    else:
+        print("Invalid answer. Please answer with y/n")
+        repeat()
+        
+while running():
+    result = input("Do you love complex numbers? ")
+    if answer.lower() == "y":
+        print("If you love complex numbers, you can be the next Stephen Hawking.")
+        loop = True;
+    if answer.lower() == "n":
+        print("I understand. You're far from being a genious.")
+        loop = False;
+    else:
+        print("Wrong Answer. Please answer with y/n")
