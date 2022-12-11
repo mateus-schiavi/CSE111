bb8f261eaf2819e9e5ea2e2b25d51e7eec2a2fcb
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-10 22:53:37 -0300

    yes

--- final_test.py
+++ final_test.py
@@ -2,9 +2,9 @@ import pytest
 from final_complex import main, repeat
 
 def test_main():
-   return main()
+   return main
     
 def test_repeat():
-    return repeat()
+    return repeat
     
 pytest.main(["-v", "--tb=line", "-rN", __file__])
