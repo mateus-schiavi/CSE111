deb1df6ad42d2649bb264e75a30e07610f4ad597
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-13 08:54:57 -0300

    yes

--- final_test.py
+++ final_test.py
@@ -2,13 +2,13 @@ import pytest
 from final_complex import main, repeat
 
 def test_main():
-    if input >= 0:
+    if input > 0:
         assert main == True
     else:
         assert main == False
     
 def test_repeat():
-    if input >= 0:
+    if input > 0:
         assert repeat == True
     else:
         assert repeat == False
