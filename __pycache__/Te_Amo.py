51f4a173c877266a746b674c99886ac13a13aa5d
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-16 20:57:41 -0300

    yes

--- __pycache__/Te_Amo.py
+++ __pycache__/Te_Amo.py
@@ -1,7 +1,5 @@
-Amor = print(input("Se quiser saber qual a mensagem, digite amor: "))
-if input == "amor":
-    print("Mãe, desde o meu primeiro dia de vida, meus dias tem sido cada vez melhores,"
+print("Mãe, desde o meu primeiro dia de vida, meus dias tem sido cada vez melhores",
           "sou grato por tudo o que a senhora me ensinou, cada passo e cada demonstração",
-          "de amor e paciência, sei que a vida precisa de altos e baixos,"
+          "de amor e paciência, sei que a vida precisa de altos e baixos",
           "pois sem isso, acabaremos morrendo."
           "Saiba que eu te amo mil milhões, e que sem você, eu não seria a pessoa que sou hoje.")
