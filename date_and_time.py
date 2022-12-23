70e349ae64cfa45f34a15aa414bb238ad13795c2
Mateus Schiavi <mtsschiavi@gmail.com>
2022-12-23 01:41:01 -0300

    yes

--- date_and_time.py
+++ date_and_time.py
@@ -33,6 +33,6 @@ while running:
 
 current_date_and_time = datetime.now()
 
-with open("tire_volume.txt", "at") as volumes:
+with open("tire_volume.txt", "at") as file:
     print(f"\n{current_date_and_time}\n{width_tire}, {aspect_tire}, {diameter_tire}, {volume}\n{phone_number}", file = tire_volumes)
     
