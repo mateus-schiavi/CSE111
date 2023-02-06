a9cba843d602c929e705ae030e90759e66f4df5f
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-06 18:17:27 -0300

    got better

--- final_test.py
+++ final_test.py
@@ -1,16 +1,15 @@
 import pytest
-from final_complex import main, repeat
+from final_complex import perform_operation
 
-def test_main():
-    result = main()
-    assert result == expected_output, f"Expected {expected_output}, but got {result}"
-    
-def test_repeat():
-    result = repeat("Hello", 3)
-    assert result == "HelloHelloHello", f"Expected 'HelloHelloHello', but got {result}"
-
-
-    
+def test_perform_operation():
+    assert perform_operation(1, 2, 3, 4, "sum") == (4 + 6j)
+    assert perform_operation(1, 2, 3, 4, "minus") == (-2 - 2j)
+    assert perform_operation(1, 2, 3, 4, "times") == (-5 + 10j)
+    assert perform_operation(1, 2, 3, 4, "division") == (0.44 + 0.08j)
+    with pytest.raises(ZeroDivisionError):
+        perform_operation(1, 2, 0, 0, "division")
+    with pytest.raises(AttributeError):
+        perform_operation(1, 2, 3, 4, "invalid")
 
     
 pytest.main(["-v", "--tb=line", "-rN", __file__])
