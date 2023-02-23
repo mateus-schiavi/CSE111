a837420fd9d83017e075c83989ebce5e86ee14a9
Mateus Schiavi <mtsschiavi@gmail.com>
2023-02-23 19:10:25 -0300

    added return

--- volume.py
+++ volume.py
@@ -1,38 +1,35 @@
 #Mateus Schiavi
 #CSE 111
 #01 Prove Milestone
-
 import math
-
 from datetime import datetime
 
-running = True;
+running = True
 
 print("Welcome to Tire Volume Calculator")
 print("|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|")
 
 def main():
     width = int(input("Enter the width of the tire in mm: "))
-    respective_ratio = int(input("Enter the aspect ratio of the tire: "))
-    respective_diameter = int(input("Enter the diameter of the wheel in inches: "))
+    aspect_ratio = int(input("Enter the aspect ratio of the tire: "))
+    diameter = int(input("Enter the diameter of the wheel in inches: "))
 
-    volume = round(((math.pi * width ** 2 * respective_ratio * (width * aspect_ratio + 2540 * respective_diameter)) / 10000000), 1)
+    volume = round(((math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000), 1)
 
     print(f"The respective volume is {volume} milliliters.")
-    repeat()
+    return (width, aspect_ratio, diameter, volume)
 
 def repeat():
     answer = input("Would you like to compute another volume (Y/N)? ")
     if answer.upper() == "Y":
         main()
-    if answer.upper() == "N":
+    elif answer.upper() == "N":
         print("The program will now end")
         exit()
     else:
         print("Error: Invalid Argument")
     
     
-
 
 while running:
     shopping = input("Would you like to buy tires with these dimensions? (yes/no) ")
@@ -48,8 +45,9 @@ while running:
     else:
         print("Please enter yes or no.")
 
-current_date_and_time = datetime.now()
+    width_tire, aspect_tire, diameter_tire, volume = main()
 
-with open("tire_volume.txt", "at") as volume:
-    print(f"\n{current_date_and_time}\n{width_tire}, {aspect_tire}, {diameter_tire}, {volume}\n{phone_number}", file= tire_volume)
-main()
+    current_date_and_time = datetime.now()
+
+    with open("tire_volume.txt", "at") as volume_file:
+        print(f"\n{current_date_and_time}\n{width_tire}, {aspect_tire}, {diameter_tire}, {volume}\n{phone_number}", file=volume_file)
