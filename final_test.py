06faf6fc8fddedddf244aa046fabd6826ad0fe05
mateus-schiavi <101905382+mateus-schiavi@users.noreply.github.com>
2023-02-24 16:12:17 -0300

    Update final_test.py

--- final_test.py
+++ final_test.py
@@ -1,11 +1,16 @@
+import cmath
 import pytest
-from final_complex import perform_operation
+
+from complex_calculator import perform_operation
+
 
 def test_perform_operation():
     assert perform_operation(1, 2, 3, 4, "sum") == (4 + 6j)
     assert perform_operation(1, 2, 3, 4, "minus") == (-2 - 2j)
     assert perform_operation(1, 2, 3, 4, "times") == (-5 + 10j)
     assert perform_operation(1, 2, 3, 4, "division") == (0.44 + 0.08j)
+    assert perform_operation(1, 2, 3, 4, "invalid") is None
+
 
 if __name__ == '__main__':
     pytest.main(["-v", "--tb=line", "-rN", __file__])
