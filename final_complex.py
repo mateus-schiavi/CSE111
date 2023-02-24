65ab745a9101318f610fd4037304e03416abcf36
mateus-schiavi <101905382+mateus-schiavi@users.noreply.github.com>
2023-02-24 16:11:41 -0300

    Update final_complex.py

--- final_complex.py
+++ final_complex.py
@@ -1,26 +1,26 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import cmath
-import pytest
 
 print("WELCOME TO COMPLEX NUMBERS CALCULATOR")
 print("|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|:|")
 
 def perform_operation(x, y, x1, y1, z):
-    sum = 0
-    minus = 0
-    times = 0
-    division = 0
     if z.lower() == "sum":
-        return complex((x + 1j*y) + (x1 + 1j*y1))
+        result = complex((x + 1j*y) + (x1 + 1j*y1))
+        return f"{x} + {y}j + {x1} + {y1}j = {result.real} + {result.imag}j"
     elif z.lower() == "minus":
-        return complex((x + 1j*y) - (x1 + 1j*y1))
+        result = complex((x + 1j*y) - (x1 + 1j*y1))
+        return f"{x} + {y}j - {x1} + {y1}j = {result.real} + {result.imag}j"
     elif z.lower() == "times":
-        return complex((x + 1j*y) * (x1 + 1j*y1))
+        result = complex((x + 1j*y) * (x1 + 1j*y1))
+        return f"({x} + {y}j) * ({x1} + {y1}j) = {result.real} + {result.imag}j"
     elif z.lower() == "division":
-        return complex((x + 1j*y) / (x1 + 1j*y1))
+        result = complex((x + 1j*y) / (x1 + 1j*y1))
+        return f"({x} + {y}j) / ({x1} + {y1}j) = {result.real} + {result.imag}j"
     else:
         return None
+
 
 def main():
     x = int(input("Enter the real part of the equation: "))
@@ -35,6 +35,7 @@ def main():
         print("Try Again, please.")
         
     repeat()
+
     
     
 def repeat():
@@ -47,12 +48,6 @@ def repeat():
         print("Invalid answer.")
         repeat()
 
-def test_perform_operation():
-    assert perform_operation(1, 2, 3, 4, "sum") == (4 + 6j)
-    assert perform_operation(1, 2, 3, 4, "minus") == (-2 - 2j)
-    assert perform_operation(1, 2, 3, 4, "times") == (-5 + 10j)
-    assert perform_operation(1, 2, 3, 4, "division") == (0.44 + 0.08j)
-    assert perform_operation(1, 2, 3, 4, "invalid") == None
         
 if __name__  == "__main__":
     main()
